ac9d3d546face5b4350e245c799f71d3c8c1f842
xxAinzxx <dominic.tosku@gmail.com>
2022-06-23 18:49:44 +0200

    Kommentare hinzugefügt

--- bin/WordReplaceHF.ps1
+++ bin/WordReplaceHF.ps1
@@ -1,32 +1,43 @@
+<# 
+Dieses Powershell Skript ersetzt den Header und Footer mit einem Template basierten Word
+Author: Dominic Tosku, Justin Urbanek, Kristyan Usarz, Luciano Zehnder
+Version: 1.0
+Letzte Änderung: 23.06.22 18:41
+#>
 
+#Pfade für das Skript
 $rootSrc = "C:\Users\Dominic\Documents\ExportInfoScript\"
 $sourceSrc = ($rootSrc + "sourceFiles\")
 $templateSrc = ($rootSrc + "templates\")
 $exportSrc = ($rootSrc + "exportedFiles\")
 
+#Geht die Word Dateien im Quel verzeichnis durch
 Get-ChildItem -Path $sourceSrc -Recurse  | ForEach-Object {
-    <# https://stackoverflow.com/questions/10727919/add-headers-and-footers-to-word-document-with-power-shell #>
-    <# https://techblog.dorogin.com/generate-word-documents-with-powershell-cda654b9cb0e #>
-    # Create a new Word application COM object
+    # Erstellt neue Word Instanz
     $WordAPI = New-Object -ComObject Word.Application;
-    # Make the Word application visible
+    # Stellt ein, ob die Datei sichtbar ist während der bearbeitung
     $WordAPI.Visible = $true;
-    # Add a new document to the application
+    # Fügt eine neue Word Datei hinzu
     $ExportedDoc = $WordAPI.Documents.Add($sourceSrc + $_);
-    # Header and footer document
+    # Öffnet das Template Dokument
     $TemplateDoc = $WordAPI.Documents.Add($templateSrc + "BBWZ.docx");
+    #Speichert die erste Sektion der Template Word Datei
     $TemplateSection = $TemplateDoc.Sections.Item(1);
-    # Get the first Section of the Document object
+    #Speichert die erste Sektion der Ziel Word Datei
     $ExportedSection = $ExportedDoc.Sections.Item(1);
-    
+
+    #Kopiert den Header des Templates und fügt ihn der Ziel Datei ein
     $TemplateSection.Headers.Item(1).Range.copy($ExportedSection.Headers.Item(1).selection.range)
     $ExportedSection.Headers.Item(1).range.PasteSpecial()
 
+    #Kopiert den Footer des Templates und fügt ihn der Ziel Datei ein
     $TemplateSection.footers.Item(1).Range.copy($ExportedSection.footers.Item(1).selection.range)
     $ExportedSection.footers.Item(1).range.PasteSpecial()
 
+    #Speichert die File unter dem selben Namen
     $ExportedDoc.SaveAs($exportSrc + $_);
     $ExportedDoc.Close();
-
+    
+    #Schliesst alle Instanzen
     $WordAPI.Quit();
 }
